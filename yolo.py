fadc4b01c839887a211f2159207e811abf072bfb
emreyilmvz <103696322+emreyilmvz@users.noreply.github.com>
2026-04-07 00:06:07 +0300

    Proje yapisi guncellendi

---
+++ yolo.py
@@ -0,0 +1,31 @@
+import cv2
+from ultralytics import YOLO
+import torch
+print(torch.cuda.is_available())
+
+model = YOLO('models/yolov8n.pt').to('cuda')  # Modeli GPU'ya taşıyarak CUDA kullanımı etkinleştirildi
+
+kamera = cv2.VideoCapture(0)
+
+print("bekleyin kamera açılıyor")
+
+while True:
+
+    ret, frame = kamera.read()  # Kameradan kare alınıyor
+    if not ret:
+        print("Kare alınamadı, çıkılıyor...")
+        break
+
+    sonuclar = model(frame, conf=0.5)  # Alınan kare YOLO modeline gönderiliyor
+
+    for sonuc in sonuclar:
+        islenmis_kare = sonuc.plot()
+
+    cv2.imshow("realtime frame", islenmis_kare)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+kamera.release()
+cv2.destroyAllWindows()
+
